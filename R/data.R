543751dfb9e8b03fc82055f18fac182f0e58b99a
Nick Klein <statistnick@tamu.edu>
2023-12-05 19:07:40 -0600

    add pbp data frames and documentation

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Texas A&M Men's Basketball data
+#'
+#' A data frame containing play-by-play data from the 2022-23 season
+#' and beginning of 2023-24 season
+#'
+#' @format ## `home.df`
+#' A data frame with 14,226 rows and 4 columns:
+#' \describe{
+#'   \item{game_id}{unique game identifier}
+#'   \item{secs_remaining}{seconds remaining in the game}
+#'   \item{home_score}{home team score}
+#'   \item{away}{away team score}
+#' }
+#' @source ESPN.com
+"home.df"
+
+#' Iowa State Men's Basketball data
+#'
+#' A data frame containing play-by-play data from the 2022-23 season
+#' and beginning of 2023-24 season
+#'
+#' @format ## `away.df`
+#' A data frame with 13,126 rows and 4 columns:
+#' \describe{
+#'   \item{game_id}{unique game identifier}
+#'   \item{secs_remaining}{seconds remaining in the game}
+#'   \item{home_score}{home team score}
+#'   \item{away}{away team score}
+#' }
+#' @source ESPN.com
+"away.df"
